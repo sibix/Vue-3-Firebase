384660e30595ee02da6b9089c0dd9980d10ef167
sibix <sibixavier@live.com  git config --global user.name sibixgit config --global user.email sibixavier@live.com>
2021-11-01 14:18:09 +0530

    retrieving data from airtable -working

--- ninja-jobs/src/views/jobs/Jobs.vue
+++ ninja-jobs/src/views/jobs/Jobs.vue
@@ -4,7 +4,16 @@
     <router-link :to=" {name: 'JobDetails', params: {id: job.id }} ">
       <h2>{{ job.title }}</h2>
     </router-link>
-    
+    <div class= "products">
+      ID -  {{ jobs.records[0].id }} <br>
+      Status - {{ jobs.records[0].fields.Status }} <br>
+      Price- {{ jobs.records[0].fields.Price }} <br>
+      Product Name - {{ jobs.records[0].fields.ProductName }} <br>
+      Quantity Available -  {{ jobs.records[0].fields.QuantityAvailable }} <br>
+      Offer price -  {{ jobs.records[0].fields.Offer-Price }} <br>
+      craeted Time - {{ jobs.records[0].createdTime }} <br>
+      Length - {{ jobs.records.length }} <br>
+    </div>
   </div>
 </template>
 
@@ -12,16 +21,21 @@
 export default {
   data() {
     return {
-      jobs: [
-        { title: 'Ninja UX Designer', id: 1, details: 'lorem' },
-        { title: 'Ninja Web Developer', id: 2, details: 'lorem' },
-        { title: 'Ninja Vue Developer', id: 3, details: 'lorem' },
-      ],
+      jobs: []
     }
+  },
+  mounted() {
+    fetch('https://api.airtable.com/v0/appQ5A4sEU3uZstcI/PRODUCTS?api_key=keyUop6Ql5CLltnDI')
+      .then(res => res.json())
+      .then(data => this.jobs = data)
+      .catch(err => console.log(err.message))
+      //console.log(this.data);
   }
 }
 </script>
 
 <style>
-
+  .products {
+    text-align: left;
+  }
 </style>
